beeef06beb525162bd72ae8f6e929b42f9542f60
morinaka.koki <21bd212@andrew.ac.jp>
2023-07-11 15:27:23 +0900

    User-conflict解消

--- db/migrate/20230628133832_create_users.rb
+++ db/migrate/20230628133832_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
+    unless table_exists?(:users)
     create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password
       t.timestamps
+      end
     end
   end
 end
