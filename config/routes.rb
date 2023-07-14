7b21d96e8fded47c15132ec9dd803c551fa7c24f
morinaka.koki <21bd212@andrew.ac.jp>
2023-07-14 11:35:39 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'home/top'
   delete '/sessions/destroy', to: 'sessions#destroy', as: 'destroy_session'
-  get '/sessions/styles.css', to: redirect('assets/styles.css')
-
 
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :show]
