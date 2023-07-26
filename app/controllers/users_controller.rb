48a8b0767fd556e3174b1f61fb88df8e147c064b
morinaka.koki <21bd212@andrew.ac.jp>
2023-07-26 10:27:49 +0900

    userコントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,40 @@
 class UsersController < ApplicationController
-    def index
-    @user = current_user # 現在ログインしているユーザーを取得
+    #def index
+    #@user = current_user # 現在ログインしているユーザーを取得
+    #end
+
+    def new
+      @user = User.new
     end
 
-    before_action :authenticate_user! # ユーザーがログインしているか確認するためのフィルタ
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            #session[:user_id] = @user.id
+            redirect_to root_path, notice: "ユーザー登録が完了しました"
+        else
+            flash.now[:alert] = "ユーザー登録に失敗しました"
+            render :new
+        end
+    end
 
     def show
-    @user = current_user # 現在ログインしているユーザーを取得
+        # ユーザー情報を取得する前に、リクエストがCSSファイルの取得リクエストであるかを確認
+        if params[:id] == "styles"
+          render plain: "", content_type: "text/css"
+        else
+          @user = current_user # ユーザー情報を取得
+        end
     end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    #def show
+    #@user = current_user # 現在ログインしているユーザーを取得
+    #end
+    #before_action :authenticate_user! # ユーザーがログインしているか確認するためのフィルタ
 end
