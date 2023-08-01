27c3688d9353d53fdf046bb7494bcd73d5c12162
morinaka.koki <21bd212@andrew.ac.jp>
2023-08-02 00:11:27 +0900

    schema更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_133832) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_01_012011) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_133832) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
