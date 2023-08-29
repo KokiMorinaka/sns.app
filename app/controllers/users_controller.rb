66e5b735cb2ddc160bdbfafda152b77f0ac982a0
morinaka.koki <21bd212@andrew.ac.jp>
2023-08-29 18:23:39 +0900

    user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,11 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+          #ユーザーが保存された後、画像をアップロードする
+          if params[:users][:image_name].present?
+            @user.update(image_name: params[:users][:image_name])
             #session[:user_id] = @user.id
+        end
             redirect_to root_path, notice: "ユーザー登録が完了しました"
         else
             flash.now[:alert] = "ユーザー登録に失敗しました"
@@ -19,13 +23,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        # ユーザー情報を取得する前に、リクエストがCSSファイルの取得リクエストであるかを確認
-        if params[:id] == "styles"
-          render plain: "", content_type: "text/css"
-        else
-          @user = current_user # ユーザー情報を取得
-        end
-    end
+      # ユーザー情報を取得する前に、リクエストがCSSファイルの取得リクエストであるかを確認
+      if params[:id] == "styles"
+        render plain: "", content_type: "text/css"
+      else
+        @user = current_user # ユーザー情報を取得
+      end
+  end
 
 
     private
