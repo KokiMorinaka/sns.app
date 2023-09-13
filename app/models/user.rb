58d2e99eddc263008c4412d0692a103d83f3c378
morinaka.koki <21bd212@andrew.ac.jp>
2023-09-13 23:27:45 +0900

    user/model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true
     validates :password, presence: true, length: { minimum: 4 }
-    validates :password_confirmation, presence: true
   
 end
