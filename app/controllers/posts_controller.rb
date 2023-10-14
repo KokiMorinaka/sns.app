ae554ddfd73498fc3192cdebd757e341a04b20c4
morinaka.koki <21bd212@andrew.ac.jp>
2023-10-14 17:27:42 +0900

    変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     before_action :set_post, only: [:edit, :update, :destroy]
-    before_action :current_user, except: [:index, :new, :edit, :update, :destroy]
+    before_action :current_user, except: [:new, :edit, :update, :destroy]
     #before_action :current_user
   
     def index
