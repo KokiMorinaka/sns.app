b7a0de6289555712738ab4d5d3f71582175bcebd
morinaka.koki <21bd212@andrew.ac.jp>
2024-03-03 18:30:28 +0900

    post更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,28 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:edit, :update, :destroy]
-    before_action :current_user, except: [:new, :edit, :update, :destroy]
-    #before_action :current_user
-  
-    def index
-      @posts = Post.all
-      #@records = Record.where("date_column <= ?", Date.today) # 今日までのデータを取得
-    end
+  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :current_user, except: [:new, :edit, :update, :destroy]
+  #before_action :current_user
+
+  def index
+    @posts = Post.all
+    @posts = Post.page(params[:page])
+    #@records = Record.where("date_column <= ?", Date.today) # 今日までのデータを取得
+  end
 
     def new
       @post = Post.new
     end
   
-    def create
-      @post = Post.new(post_params)
-  
-      if @post.save
-        redirect_to posts_path
-      else
-        render :new
-      end
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      Notification.create(post: @post, title: @post.title, content: @post.content, recruitment_number: @post.recruitment_number)
+      redirect_to @post, notice: '投稿が成功しました。'
+    else
+      render :new
     end
+  end
   
     def edit
       @post = Post.find(params[:id])
